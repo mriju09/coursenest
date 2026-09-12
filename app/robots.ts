f154016f754b53ef9365f4dd71cd61635e157f88
Riju Mondal <mriju943@gmail.comL>
2026-09-13 01:03:45 +0700

    Add robots.txt configuration

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://coursenest-snowy.vercel.app/sitemap.xml",
+  };
+}
