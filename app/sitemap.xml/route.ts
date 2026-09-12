d3f5e762e8ecc7d0ff8f2a743e2643d4ee6db9cc
Riju Mondal <mriju943@gmail.comL>
2026-09-13 00:23:00 +0700

    Update sitemap URL

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -2,7 +2,7 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://coursenest.in/</loc>
+    <loc>https://coursenest-snowy.vercel.app/</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
